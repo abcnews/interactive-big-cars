1bd0a840bff62d51abb8a907dd4a467322a44430
Joshua Byrd <phocks@gmail.com>
2026-08-28 16:53:00 +1000

    Loader script do a Promise

--- public/dev-loader.js
+++ public/dev-loader.js
@@ -1,6 +1,22 @@
 (function () {
-	const script = document.createElement("script");
-	script.type = "module";
-	script.src = "http://localhost:5173/src/main.ts";
-	document.head.appendChild(script);
+	function loadScript(src) {
+		return new Promise(function (resolve, reject) {
+			let script = document.createElement("script");
+			script.type = "module";
+			script.src = src;
+
+			script.onload = () => resolve(script);
+			script.onerror = () => reject(new Error(`Script load error for ${src}`));
+
+			document.head.append(script);
+		});
+	}
+
+	loadScript("http://localhost:5173/src/main.ts")
+		.then(function () {
+			console.log("Dev script loaded...");
+		})
+		.catch(function (error) {
+			console.error("Dev script not loaded...");
+		});
 })();
