452a88727cf12d883dd4bcfae2efad8ccc799309
Joshua Byrd <phocks@gmail.com>
2026-09-09 13:52:17 +1000

    Change version

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # interactive-big-cars
 
 A story about big cars
+
+### Note:
+
+Don't forget to change the version number in `public/interactive-loader.js` on version bump.
--- public/interactive-loader.js
+++ public/interactive-loader.js
@@ -1,6 +1,8 @@
 (async function () {
+	// TODO: Change version on publish or use the currentScript src method
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	const baseUrl =
-		"https://www.abc.net.au/res/sites/news-projects/interactive-big-cars/dist/";
+		"https://www.abc.net.au/res/sites/news-projects/interactive-big-cars/1.0.0/";
 	const manifestUrl = `${baseUrl}/.vite/manifest.json`;
 
 	const manifest = await fetch(manifestUrl).then((r) => r.json());
